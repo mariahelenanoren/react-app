9a9f29da574e2117f0f2b6533791b1d8f8cae1cf
mariahelenanoren <marialinder97@hotmail.com>
2021-02-17 12:11:18 +0100

    added go back button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,29 @@
 import "../css/App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { renderResults, renderSearch } from "../actions";
 
 function Header() {
+  const state = useSelector((state) => state.mainState);
+  const dispatch = useDispatch();
+
   return (
     <div className="App-header">
       <h1 className="App-name">
         <a href="./">Simply delicious</a>
       </h1>
+      <button
+        onClick={() => {
+          if (state === "recipe") {
+            dispatch(renderResults());
+          } else if (state === "searchResults") {
+            dispatch(renderSearch());
+          } else {
+            dispatch(renderSearch());
+          }
+        }}
+      >
+        Go back
+      </button>
     </div>
   );
 }
