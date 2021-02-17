f4f16568fd217c2baa5dfff79189b14ff871559f
mariahelenanoren <marialinder97@hotmail.com>
2021-02-17 09:16:57 +0100

    recipe card component now loops

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-import "../css/RecipeCard.css"
+import "../css/RecipeCard.css";
 
 class RecipeCard extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
+    console.log(this.props);
     return (
       <div className="recipe-card">
-        <img src={this.props.img} />
-        <h1>{this.props.title}</h1>
+        <img src={this.props.items.img} />
+        <h1>{this.props.items.title}</h1>
       </div>
     );
-    }
+  }
 }
 
 export default RecipeCard;
